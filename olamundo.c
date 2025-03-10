a89edf76f07ab0dfed6cabe527e12307865052fa
katelynvieira <kcrisrock@gmail.com>
2025-03-10 11:32:19 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main(){
+
+// Sintaxe padrão printf
+  printf ("Olá, Mundo!");
+//printf("texto com formatação", variavel1, variavel2,...);
+//printf("Olá, Mundo!"/n);
+
+}
